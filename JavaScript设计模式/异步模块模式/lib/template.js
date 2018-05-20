b65e10ae38c0b49c122aa77f432424c1f9e08e11
Yang-yu-dong <youngyudong@foxmail.com>
2018-05-21 03:21:11 +0800

    2018/5/21

---
+++ JavaScript设计模式/异步模块模式/lib/template.js
@@ -0,0 +1,7 @@
+F.module('lib/template',function () {
+    var _TplEngine = function () {},
+    _getTpl = function () {},
+    _dealTpl = function () {},
+    _compileTpl = function () {};
+    return _TplEngine;
+});
